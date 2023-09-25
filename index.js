f91e8de868f85ab6f77a824cbba645b38b247033
Eckra Wangui <+>
2023-09-25 11:47:10 +0300

    Completed message

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph")
+
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift( "Bob")
+}
+
+
+function destructivelyRemoveLastCat(){
+    cats.pop("Garfield")
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift("Milo")
+}
+
+function appendCat(name){
+
+    const appendCat = [...cats,"Broom"];
+    return appendCat
+    
+}s
+
+function prependCat(name){
+    const prependCat = ["Arnold", ...cats];
+  return prependCat
+}
+
+function  removeLastCat(){
+    const removeLastCat=cats.slice(0, 2);
+    return removeLastCat
+}
+
+function removeFirstCat(){
+    const removeFirstCat= cats.slice(1);
+    return removeFirstCat
+}
+
+
